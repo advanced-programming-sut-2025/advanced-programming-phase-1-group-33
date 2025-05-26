3f2e9413397d107a4ad9fdf39f08aa0f06c00705
Aeen <aeenkarimian@gmail.com>
2025-05-27 02:12:11 +0330

    Some Minor Changes here in the Artistan machine

--- console-game/src/main/java/com/yourgame/model/ManuFactor/ArtisanMachine.java
+++ console-game/src/main/java/com/yourgame/model/ManuFactor/ArtisanMachine.java
@@ -10,14 +10,14 @@ import com.yourgame.model.*;
 import java.util.HashMap;
 
 public abstract class ArtisanMachine {
+    protected ArtisanGood producingGood;
     protected TimeSystem timeOfRequest;
     protected HashMap<ArtisanGood, TimeFrame> processingTimes;
-    protected ArtisanGood producingGood;
 
     public ArtisanMachine() {
-        processingTimes = new HashMap<>();
         timeOfRequest = null;
         producingGood = null;
+        processingTimes = new HashMap<>();
     }
 
     public void use() {
@@ -38,23 +38,27 @@ public abstract class ArtisanMachine {
     }
 
     public Response isReady() {
-        if (timeOfRequest == null)
+        if (timeOfRequest == null) {
             return new Response(false, "You don't have any artisan goods in machine yet!!");
-        int todayDate = App.getGameState().getGameTime().getDay();
+        }
         int todayHour = App.getGameState().getGameTime().getHour();
+        int todayDate = App.getGameState().getGameTime().getDay();
         if (App.getGameState().getGameTime().getSeason() != timeOfRequest.getSeason()) {
             todayDate += 28;
             if (timeOfRequest.getDay() + processingTimes.get(producingGood).getDays() < todayDate ||
                     timeOfRequest.getDay() + processingTimes.get(producingGood).getDays() == todayDate &&
-                            timeOfRequest.getHour() + processingTimes.get(producingGood).getHours() <= todayHour)
+                            timeOfRequest.getHour() + processingTimes.get(producingGood).getHours() <= todayHour) {
+
                 return new Response(true, "Your product is Ready.");
+            }
         }
         return new Response(false, "Your product is Not Ready.");
     }
 
     public static ArtisanMachine getArtisanMachineByRecipe(CraftingRecipes recipe) {
-        if (recipe == null)
+        if (recipe == null){
             return null;
+        }
         return switch (recipe) {
             case Furnace -> new Furnace();
             case CharcoalKiln -> new CharcoalKiln();
