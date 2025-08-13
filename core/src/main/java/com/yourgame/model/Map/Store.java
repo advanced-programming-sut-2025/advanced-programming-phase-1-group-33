c4d2204bdadde5c5eee02ed11cafa16a35560822
sepimoti <sepehrmotavallian@gmail.com>
2025-08-13 11:40:22 +0330

    Add: buy and sell zone in Store

--- core/src/main/java/com/yourgame/model/Map/Store.java
+++ core/src/main/java/com/yourgame/model/Map/Store.java
@@ -3,30 +3,49 @@ package com.yourgame.model.Map;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Rectangle;
 import com.yourgame.model.UserInfo.Player;
 
 public class Store extends Map {
-    private final MapObjects tradeObjects;
+    private final MapObjects tradeZoneObjects;
 
     public Store(String name, String pathToTmx) {
         super(name, pathToTmx);
-        this.tradeObjects = tiledMap.getLayers().get("Trade").getObjects();
+        this.tradeZoneObjects = tiledMap.getLayers().get("Trade").getObjects();
     }
 
     public boolean isPlayerInTradeZone(Player player) {
-        for (MapObject object : tradeObjects) {
-            if (object instanceof RectangleMapObject rectObj) {
-                float x = rectObj.getRectangle().x;
-                float y = rectObj.getRectangle().y;
-                float width = rectObj.getRectangle().width;
-                float height = rectObj.getRectangle().height;
-
-                if (player.playerPosition.x >= x && player.playerPosition.x <= x + width
-                    && player.playerPosition.y >= y && player.playerPosition.y <= y + height) {
-                    return true;
-                }
+        for (MapObject object : tradeZoneObjects) {
+            if (object instanceof RectangleMapObject rectObj && overLaps(rectObj.getRectangle(), player)) {
+                return true;
             }
         }
         return false;
+    }
+
+    public boolean isPlayerInBuyZone(Player player) {
+        MapObject object = tradeZoneObjects.get("buy");
+        if (object instanceof RectangleMapObject rectObj) {
+            return overLaps(rectObj.getRectangle(), player);
+        }
+        return false;
+    }
+
+    public boolean isPlayerInSellZone(Player player) {
+        MapObject object = tradeZoneObjects.get("sell");
+        if (object instanceof RectangleMapObject rectObj) {
+            return overLaps(rectObj.getRectangle(), player);
+        }
+        return false;
+    }
+
+    private boolean overLaps(Rectangle rectangle, Player player) {
+        float x = rectangle.x;
+        float y = rectangle.y;
+        float width = rectangle.width;
+        float height = rectangle.height;
+
+        return (player.playerPosition.x >= x && player.playerPosition.x <= x + width
+             && player.playerPosition.y >= y && player.playerPosition.y <= y + height);
     }
 }
