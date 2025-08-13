b803193c49ec440e7e74927d12b0391e75fd725c
Amirhossein <amirhmoll84@gmail.com>
2025-08-13 12:19:06 +0330

    Finish Purchase

--- core/src/main/java/com/yourgame/model/Map/Store/Store.java
+++ core/src/main/java/com/yourgame/model/Map/Store/Store.java
@@ -3,33 +3,21 @@ package com.yourgame.model.Map.Store;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
-<<<<<<< HEAD:core/src/main/java/com/yourgame/model/Map/Store.java
-import com.badlogic.gdx.math.Rectangle;
-import com.yourgame.model.UserInfo.Player;
-
-public class Store extends Map {
-    private final MapObjects tradeZoneObjects;
-=======
 import com.yourgame.model.App;
 import com.yourgame.model.Map.Map;
 import com.yourgame.model.UserInfo.Player;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Store extends Map {
-    private final MapObjects tradeObjects;
+    private final MapObjects tradeZoneObjects;
     protected final int startHour;
     protected final int endHour;
->>>>>>> shop:core/src/main/java/com/yourgame/model/Map/Store/Store.java
-
 
     public Store(String name, String pathToTmx, int startHour, int endHour) {
         super(name, pathToTmx);
-<<<<<<< HEAD:core/src/main/java/com/yourgame/model/Map/Store.java
         this.tradeZoneObjects = tiledMap.getLayers().get("Trade").getObjects();
-=======
-        this.tradeObjects = tiledMap.getLayers().get("Trade").getObjects();
         this.startHour = startHour;
         this.endHour = endHour;
->>>>>>> shop:core/src/main/java/com/yourgame/model/Map/Store/Store.java
     }
 
     public boolean isPlayerInTradeZone(Player player) {
@@ -40,7 +28,6 @@ public class Store extends Map {
         }
         return false;
     }
-<<<<<<< HEAD:core/src/main/java/com/yourgame/model/Map/Store.java
 
     public boolean isPlayerInBuyZone(Player player) {
         MapObject object = tradeZoneObjects.get("buy");
@@ -67,8 +54,7 @@ public class Store extends Map {
         return (player.playerPosition.x >= x && player.playerPosition.x <= x + width
              && player.playerPosition.y >= y && player.playerPosition.y <= y + height);
     }
-=======
-    
+
     public int getEndHour() {
         return endHour;
     }
@@ -82,5 +68,4 @@ public class Store extends Map {
     }
 
     public void loadInventory() {}
->>>>>>> shop:core/src/main/java/com/yourgame/model/Map/Store/Store.java
 }
